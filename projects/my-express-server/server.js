b60b0ae0607523ffdf707b6589f16db19a25ed59
Michael Walter <michael_patrick.walter@outlook.com>
2020-11-19 22:41:54 -0800

    nodeJS up and running, express implemented, nodemon implemented, first three endpoints fully functional.

--- projects/my-express-server/server.js
+++ projects/my-express-server/server.js
@@ -13,6 +13,18 @@ const express = require('express');
 
 const app = express();
 
+app.get("/", (req,res) => {
+    res.send("<h1>Hello World!</h1>")
+})
+
+app.get("/contact", (req,res) => {
+    res.send("<h1>Contact me at Mail@email.com</h1>")
+})
+
+app.get("/about", (req,res) => {
+    res.send("<h1>It's me, Michael the Developer!</h1>")
+})
+
 app.listen(5500, () => {
     console.log("Server started on port 5500")
 });
