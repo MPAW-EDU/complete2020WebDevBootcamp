c9e1110063394d2c2babb8cbe5ed77001730b520
Michael Walter <michael_patrick.walter@outlook.com>
2020-11-22 00:24:51 -0800

    External weather api call working. Figured out how to pase the data and will beging working on implementing it into a new website tomorrow / later today.

--- projects/WeatherProject/app.js
+++ projects/WeatherProject/app.js
@@ -13,7 +13,16 @@ app.get("/", (req,res) => {
     const url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=6e54593ffa740348e4ee9c91a9a2dab4"
 
     https.get(url, (response) => {
-        console.log(response);
+        console.log(response.statusCode);
+
+        response.on("data", (data) => {
+            const weatherData = JSON.parse(data);
+
+            const temp = weatherData.main.temp;
+            const weatherDesc = weatherData.weather[0].description;
+            console.log(weatherDesc)
+
+        })
     })
 
     res.send("Server is working!")
