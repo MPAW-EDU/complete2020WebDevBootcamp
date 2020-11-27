9dc777f6a1ad862d09180dc34c156e79baec640b
Michael Walter <michael_patrick.walter@outlook.com>
2020-11-27 10:53:55 -0800

    Implemented express static to allow custom styles and images to maintain their linking. Built out form, linked it to backend API, now starting to setup API call to mailchimp to build a newsletter email list.

--- projects/Newsletter-Signup/app.js
+++ projects/Newsletter-Signup/app.js
@@ -7,8 +7,22 @@ const PORT = 5500;
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 app.get("/", (req,res) => {
     res.sendFile(`${__dirname}/signup.html`);
+})
+
+app.post("/signup", (req,res) => {
+
+    let firstName = req.body.fName;
+    let lastName = req.body.lName;
+    let email = req.body.email;
+
+    console.log(req.body);
+    res.send(`Thank you for signing-up ${firstName}, your first newsletter will arrive at ${email} shortly.`);
+
 })
 
 app.listen(PORT, () => {
