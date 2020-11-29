530c2b6ab9cc6c0dd8137b7daea6a0b698b6fe99
Michael Walter <michael_patrick.walter@outlook.com>
2020-11-28 17:31:38 -0800

    Building in mailchimp functionality to make a mailing list.

--- projects/Newsletter-Signup/app.js
+++ projects/Newsletter-Signup/app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
 const PORT = 5500;
 
 const app = express();
@@ -16,15 +17,43 @@ app.get("/", (req,res) => {
 
 app.post("/signup", (req,res) => {
 
-    let firstName = req.body.fName;
-    let lastName = req.body.lName;
-    let email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAE: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    // Insert Here
+
+    const request = https.request(url, options, (response) => {
+        response.on("data", (data) => {
+            console.log(JSON.parse(data));
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
 
     console.log(req.body);
-    res.send(`Thank you for signing-up ${firstName}, your first newsletter will arrive at ${email} shortly.`);
+    // res.send(`Thank you for signing-up ${firstName}, your first newsletter will arrive at ${email} shortly.`);
 
 })
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}.`)
 })
+
+
