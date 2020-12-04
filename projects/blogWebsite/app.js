5c58d419714c5aa1c8bb023af23b4e85a36be0a5
Michael Walter <michael_patrick.walter@outlook.com>
2020-12-04 09:47:52 -0800

    Working through main solution.

--- projects/blogWebsite/app.js
+++ projects/blogWebsite/app.js
@@ -1,76 +1,33 @@
+//jshint esversion:6
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const PORT = 3000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
-let updateID = null;
-
-let posts = [
-    {
-        'title': 'Day 1',
-        'id': '1',
-        'author': "MPAW",
-        'date': "12/1/2020",
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    },
-    {
-        'title': 'Day 2',
-        'id': '2',
-        'author': "MPAW",
-        'date': "12/2/2020",
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    }
-]
-
-const getDate = () => {
-    const today = new Date();
-
-    const options = {
-        month: "numeric",
-        day: "numeric",
-        year: "numeric"
-    }
-
-    const day = today.toLocaleString('en-US', options);
-
-    return day;
-}
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-app.set('view engine', 'ejs');
 
 
 
-app.get("/", (req,res) => {
-    res.render("main", {postList: posts});
-    console.log(getDate())
-})
-
-app.get("/create", (req,res) => {
-    res.render("create");
-})
-
-app.post("/create", (req,res) => {
-    const newPost = {
-        'title': `${req.body.title}`,
-        'id': `${posts.length + 1}`,
-        'author': "MPAW",
-        'date': `${getDate()}`,
-        'content': `${req.body.content}`
-    }
-    posts.push(newPost)
-
-    res.redirect("/")
-})
-
-app.get("/update", (req,res) => {
-    console.log("Success!")
-})
 
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
