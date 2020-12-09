844ff6a3cf6de7b24ef910d8f7c08da3e7008702
Michael Walter <michael_patrick.walter@outlook.com>
2020-12-09 10:22:04 -0800

    Built out full CRUD in mongoose, all tested and working, this example study project is done.

--- projects/FruitsProject/app.js
+++ projects/FruitsProject/app.js
@@ -10,37 +10,70 @@ mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, 
 // Insert data to the DB, first by creating a new schema
 
 const fruitSchema = new mongoose.Schema ({
-    name: String,
-    rating: Number,
+    name: {
+        type: String,
+        required: [true, "Please check your entered name, no name was specified!"]
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10
+    },
     review: String
 });
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
-const fruit = new Fruit ({
-    name: "Apple",
-    rating: 7,
-    review: "Pretty solid as a fruit."
-});
+// const fruit = new Fruit ({
+//     name: "Apple",
+//     rating: 34,
+//     review: "Pretty solid as a fruit."
+// });
 
-const kiwi = new Fruit ({
-    name: "Kiwi",
-    score: 10,
-    review: "The bestest fruit!"
-})
-
-const orange = new Fruit ({
-    name: "Orange",
-    score: 4,
-    review: "Too sour for me"
-})
-
-const banana = new Fruit ({
-    name: "Banana",
-    score: 3,
-    review: "A weird texture"
-})
 /**
+ *   Using validation above to test for missing name
+ */
+// const fruit = new Fruit ({
+//     rating: 10,
+//     review: "Peaches are delicious!"
+// })
+
+// const kiwi = new Fruit ({
+//     name: "Kiwi",
+//     score: 10,
+//     review: "The bestest fruit!"
+// })
+
+// const orange = new Fruit ({
+//     name: "Orange",
+//     score: 4,
+//     review: "Too sour for me"
+// })
+
+// const banana = new Fruit ({
+//     name: "Banana",
+//     score: 3,
+//     review: "A weird texture"
+// })
+
+const pineapple = new Fruit ({
+    name: "Pineapple",
+    rating: 9,
+    review: "I like it on my pizza!"
+})
+
+const dragonFruit = new Fruit ({
+    name: "Dragon Fruit",
+    rating: 10,
+    review: "I could eat these till I pop!"
+})
+
+
+
+// dragonFruit.save().then(console.log("Dragon Fruit saved!"));
+// pineapple.save().then(console.log("pineapple saved!"));
+/**
+ *  Create
  *  Add mutliple entries and catch errors
  */ 
 // Fruit.insertMany([kiwi, orange, banana], function(err){
@@ -51,35 +84,109 @@ const banana = new Fruit ({
 //     }
 // })
 // Add a single entry
-// fruit.save().then(console.log("Fruit Saved"))
+// .save().then(console.log("Fruit Saved"))
 
 const personSchema = new mongoose.Schema ({
     name: String,
-    age: Number
+    age: Number,
+    favoriteFruit: fruitSchema
 });
-
+dragonFruit.save().then(console.log("Fruit Save Success!"));
 const Person = mongoose.model("Person", personSchema);
 
-const person = new Person ({
-    name: "John",
-    age: 37
+/**
+ *  Updating a record, one
+ */
+Person.updateOne({name: "John"}, {favoriteFruit: dragonFruit}, function(err){
+    if(err){
+        console.log(err);
+    } else {
+        console.log("Update Successful!");
+    }
 })
 
-// person.save().then(console.log("Person Saved"))
+/**
+ *  Creates a relational entry to another piece of data,
+ *  in another collection or table
+ *  AN EMBEDDED DOC, Links by ID
+ */
+// const person = new Person ({
+//     name: "Amy",
+//     age: 12,
+//     favoriteFruit: pineapple
+// })
+const person = new Person ({
+    name: "John",
+    age: 37,
+})
 
 /**
+ *  Save a single record / object
+ */
+// person.save().then(console.log("Person Saved"));
+
+/**
+ *  Read
  * Tap into the fruits DB and pull the data
  */
 
- Fruit.find(function(err, fruits) {
-     if(err){
-         console.log(err)
-     } else {
+//  Fruit.find(function(err, fruits) {
+//      if(err){
+//          console.log(err)
+//      } else {
 
-        mongoose.connection.close();
+//         mongoose.connection.close();
 
-         fruits.forEach(fruit => {
-             console.log(fruit.name)
-         });
-     }
- })
+//          fruits.forEach(fruit => {
+//              console.log(fruit.name)
+//          });
+//      }
+//  })
+
+/**
+ *  Update
+ */
+//  Fruit.updateOne({_id:"5fd101cc37ec270ba8301b20"}, {name: "Peach"}, function(err) {
+//      if(err){
+//          console.log(err)
+//      } else {
+//          console.log("Update Successful!");
+//      }
+//      mongoose.connection.close();
+//  })
+
+/**
+ *  Update John, give him a favorite fruit
+ */
+//  Fruit.updateOne({name: "John"}, {favoriteFruit: dragonFruit}, function(err) {
+//      if(err){
+//          console.log(err)
+//      } else {
+//          console.log("Updated fruit rating Successfully!");
+//      }
+//      mongoose.connection.close();
+//  })
+
+/**
+ *  Delete, one
+ */
+// Fruit.deleteOne({name: "Peach"}, function(err) {
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log("Deletion Successful!");
+//     }
+//     mongoose.connection.close();
+// })
+
+/**
+ *  Delete, many
+ */
+// Person.deleteMany({name: "John"}, function(err) {
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log("Deletion of many has beeen successful!");
+//     }
+//     mongoose.connection.close();
+// });
