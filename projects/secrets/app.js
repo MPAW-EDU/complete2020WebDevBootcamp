3429988a5267ee72d00d7c064b85a833000f5acf
Michael Walter <michael_patrick.walter@outlook.com>
2020-12-23 20:43:54 -0800

    Added the endpoints for home, login, register. All are working, and I added a status code for each one.

--- projects/secrets/app.js
+++ projects/secrets/app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const { response } = require('express');
 
 let PORT = process.env.PORT;
 if(PORT == null || PORT == ""){
@@ -12,10 +13,21 @@ if(PORT == null || PORT == ""){
 const app = express();
 
 app.use(express.static("public"));
-app.set('view enginne', ' ejs');
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 
+app.get("/", (req,res) => {
+    res.status("200").render(("home"))
+})
+
+app.get("/login", (req,res) => {
+    res.status("200").render(("login"))
+})
+
+app.get("/register", (req,res) => {
+    res.status("200").render(("register"))
+})
 
 
 app.listen(PORT, () => {
