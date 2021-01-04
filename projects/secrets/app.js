c0d8385872e599791af7bc8349dd992689eab7d5
Michael Walter <michael_patrick.walter@outlook.com>
2021-01-04 13:23:49 -0800

    Secrets: Working on additional challenge of implementing OAuth for Facebook login.

--- projects/secrets/app.js
+++ projects/secrets/app.js
@@ -146,6 +146,31 @@ passport.use(new GoogleStrategy({
     }
 ));
 
+/**
+ *  Facebook Login Strategy
+ */
+passport.use(
+    new FacebookStrategy(
+        {
+            clientID: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+            callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+            profileFields: ["email", "name"]
+        },
+        function(accessToken, refreshToken, profile, done){
+            const { email, first_name, last_name } = profile._json;
+            const userData = {
+                email,
+                firstName: first_name,
+                lastName: last_name
+            };
+            User.findOrCreate({ facebookId: profile.id }, (err,user) => {
+                return done(null, profile);
+            });
+        }
+    )
+);
+
 
 app.get("/", (req,res) => {
     res.status("200").render(("home"))
