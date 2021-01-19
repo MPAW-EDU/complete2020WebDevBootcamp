7be18192522fa57204a568e57b76ed33fb6df09c
Michael Walter <michael_patrick.walter@outlook.com>
2021-01-18 21:55:06 -0800

    conditional-rendering: build out success and login components and leveraged their view on a bool var called isLoggedIn, the ran it all together in a function called checkLoggedIn.

---
+++ projects/conditional-rendering/src/components/Success.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+
+const Success = () => {
+    return (
+        <div>
+            <h1>Welcome, You've logged in!</h1>
+        </div>
+    )
+};
+
+export default Success;
