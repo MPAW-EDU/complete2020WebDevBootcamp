7e7368e542e5a64526dcef5884a6d8c7b2a68628
Michael Walter <michael_patrick.walter@outlook.com>
2021-01-18 22:38:35 -0800

    conditional-rendering-practice: implemented conditional rendering for confirm email and changing the form button text. In addition I learned a new way to implement conditions for this purpose.

---
+++ projects/conditional-rendering-practice/src/components/Form.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Form({registered}) {
+  return (
+    <form className="form">
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      {
+        !registered &&
+        <input type="password" placeholder="Confirm Password" />
+      }
+      <button type="submit">{registered?"Login":"Register"}</button>
+    </form>
+  );
+}
+
+export default Form;
