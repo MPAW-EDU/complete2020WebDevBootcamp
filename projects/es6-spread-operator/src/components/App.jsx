e273a332db0fc7ee3874c45640b35a5cb28a07f1
Michael Walter <michael_patrick.walter@outlook.com>
2021-01-21 12:21:13 -0800

    implemented a more advanced method for the change handler, truly a mind boggling solution which I need to learn more about.

---
+++ projects/es6-spread-operator/src/components/App.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    // Wrap it in a parenthesis to tell the compiler that it's
+    // an object inside and the body of the function.
+    setContact(prevState => ({
+
+        ...prevState,
+        // By adding brackets on the outside when extract
+        // the value of the variable to use when
+        // constructing the new object for updating state.
+        [name]: value
+      
+    }))
+
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          value={contact.lName}
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          value={contact.email}
+          placeholder="Email"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
